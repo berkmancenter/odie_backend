c654f4dba7525a63f588bba8f973f25e33551be5
Andromeda Yelton <andromeda.yelton@gmail.com>
2019-05-24 12:53:58 -0400

    Make login pretty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  layout 'admin/application'
+
+  def after_sign_in_path_for(resource)
+  	stored_location_for(resource) || admin_root_path
+  end
 end
