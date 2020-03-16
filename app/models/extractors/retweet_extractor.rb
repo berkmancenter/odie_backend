ecd0f3dffb6fdcaddf2e7d291a38195fec70ac5d
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2020-03-16 17:57:01 +0100

    Add link to retweet to top_retweets

--- app/models/extractors/retweet_extractor.rb
+++ app/models/extractors/retweet_extractor.rb
@@ -4,9 +4,29 @@ class RetweetExtractor < Extractor
   def extract
     @tweets.map { |tweet| all_nested(:retweeted_status, tweet) }
            .flatten
-           .map { |tweet| tweet.attrs[:full_text] }
-           .each do |tweet|
-             @all_things[tweet] += 1
+           .map do |tweet|
+             {
+               text: tweet.attrs[:full_text],
+               link: tweet.uri.to_s
+             }
            end
+           .each do |tweet|
+             if @all_things[tweet[:text]].zero?
+               @all_things[tweet[:text]] = {
+                 count: 1,
+                 link: tweet[:link]
+               }
+             else
+               @all_things[tweet[:text]][:count] += 1
+             end
+           end
+  end
+
+  def collate
+    # Only report the top N ranks (including ties), and don't report anything
+    # below the threshold.
+    min_count = @all_things.values.sort.last(TOP_N)[0]
+    @all_things.reject { |k, v| v < [min_count, THRESHOLD].max }
+    @all_things.transform_values(&:to_json)
   end
 end
